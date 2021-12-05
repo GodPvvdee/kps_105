902f6fdd3ae7ea73813dc8016b217096c9062002
Pvvdee <GodPvvdee@gmail.com>
2021-12-05 12:44:19 +0800

    banknii hvv bodoh bodlogo

--- loop/function_example.js
+++ loop/function_example.js
@@ -7,22 +7,20 @@ let income = 1000000;
 let rate = 0.13;
 // month = hugatsaa(saraar)
 const getSavings = (month) => {
-    let result = income*rate*month + income;
+    let result = income * rate *month + income;
     return result;
 }
 // hadgalamjiin dvngees 10% tatwar tootsdog tul vvniig shuudah function heregtei
 // Ene function dotor hadgalamjiin niit dvng tootsooloh getSavings() functions ashiglana
-const getTax = (month) =>{
-    // hadgalamjiin tatwar 10%
-    let tax = getSavings(month) *0.1;
+const getTax = (month) => {
+    let tax = getSavings(month) * 0.1;
     return tax;
 }
 // etsest n hadgalamjii etsesiiin vr dvng tootsoh function bichiy tatwaraas omnoh hadgalamjiin 
 // dvngees tatwariig hassanaar ene function vr dvn bodogdono
-const totalSavings = (month) =>{
+ const totalSavings = (month) =>{
     return getSavings(month) - getTax(month);
-};
-
+ };
 // shalgay
 // hugatsaa 6 sar
 document.write( 'Татвараас өмнөх хадгаламжийн дүнг:' + getSavings(6) + "<br>");
